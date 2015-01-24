a859bb8ef44d1292d0caa8708b614be309a99360
Mudassir <mudassir.dev@gmail.com>
2015-01-25 01:57:23 +0530

    Implement y = 15*x^2 graph using the concept of functions as bindings

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
 window.onload = function() {
+    var data = [];
 
+    var next = function(x) {
+        return 15 + x * x;
+    };
+
+    var newData = function() {
+        data.push(next);
+        return data;
+    };
+
+
+    function render() {
+        var selection = d3.select('#container')
+                            .selectAll('div')
+                            .data(newData);
+
+        selection.enter().append('div').append('span');
+
+        selection.exit().remove();
+
+        selection.attr('class', 'v-bar')
+                .style('height', function(d, i) {
+                    return d(i) + 'px';
+                })
+            .select('span')
+                .text(function(d, i) {
+                    return d(i);
+                });
+    }
+
+    setInterval(function() {
+        render();
+    }, 1500);
+
+    render();
 }
